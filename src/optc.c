9aadd3f90886c399f6af8a3d704ffadd482e29d3
clement <clement1.ruat@epitech.eu>
2020-03-23 01:56:05 +0100

    [ADD] adding tiegstr and have setup config

--- src/optc.c
+++ src/optc.c
@@ -5,47 +5,62 @@
 ** optc
 */
 
+#include <term.h>
+#include <curses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <ncurses.h>
 #include "tetris.h"
 #include "struct.h"
 
 void init_value(touch_t *touch)
 {
-    touch->touching = malloc((sizeof(char*)) * 8);
-    touch->touching[0] = "0";
-    touch->touching[1] = "0";
-    touch->touching[2] = "0";
-    touch->touching[3] = "0";
-    touch->touching[4] = "0";
-    touch->touching[5] = "0";
-    touch->touching[6] = "0";
-    touch->touching[7] = "0";
+    int terminal = 0;
+
+    setupterm(NULL, 0, &terminal);
+    touch->touching = malloc((sizeof(char *)) * 9);
+    touch->touching[left] = "0";
+    touch->touching[right] = "0";
+    touch->touching[turn] = "";
+    touch->touching[drop] = "0";
+    touch->touching[quit] = "q";
+    touch->touching[pose] = "0";
+    touch->touching[level] = "1";
+    touch->touching[size] = "20*10";
+    printf("%s\n", tigetstr("kcub1"));
     touch->print_debug = 0;
     touch->next_hide = 0;
 }
 
+void choosing2(int opt, touch_t *touch)
+{
+    switch(opt) {
+    case 'L':
+        touch->touching[level] = optarg;
+        break;
+    }
+}
+
 void choosing(int opt, touch_t *touch)
 {
+    if (opt == -1 && touch->print_debug == 1)
+        display_debug_mode(touch);
     switch(opt) {
     case 'h':
         helper();
         break;
-    case 'L':
-        touch->touching[0] = optarg;
-        break;
-    case 'l':
-        touch->touching[1] = optarg;
-        break;
-    case 'w':
-        touch->next_hide = 1;
-        break;
     case 'D':
         touch->print_debug = 1;
         break;
-    }        
+    case 'w':
+        touch->next_hide = 0;
+        break;
+    default:
+        choosing2(opt, touch);
+        break;
+    }
 }
 
 void find_arg(int ac, char **av, touch_t *touch)
@@ -66,11 +81,9 @@ void find_arg(int ac, char **av, touch_t *touch)
         {"without-next", no_argument, NULL, 'w'},
         {"debug", no_argument, NULL, 'D'}
     };
-    init_value(touch);
+
     while (opt != -1) {
         opt = getopt_long(ac, av, string, long_options, &option_index);
         choosing(opt, touch);
     }
-    if (opt == -1 && touch->print_debug == 1)
-        display_debug_mode(touch);
 }
