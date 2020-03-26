21bf2a88e1c58173df566825501ad83b43db1d36
clement <clement1.ruat@epitech.eu>
2020-03-26 14:24:14 +0100

    [ADD] enlevage de l'auto print debug

--- src/main.c
+++ src/main.c
@@ -11,10 +11,8 @@
 int main(int ac, char **av)
 {
     touch_t touch;
-    int ret;
 
     init_value(&touch);
     find_arg(ac, av, &touch);
-    ret = display_debug_mode(&touch);
-    return (ret == 0 ? 0: 84);
+    return 0;
 }
