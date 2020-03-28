6da5c60ac7d595d34b4f98d12771412759dfdb71
clement <clement1.ruat@epitech.eu>
2020-03-28 05:41:05 +0100

    [ADD] selection mode of keys

--- src/key_press.c
+++ src/key_press.c
@@ -8,6 +8,10 @@
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <ncurses.h>
+#include "my.h"
+#include "tetris.h"
+#include "struct.h"
 
 int key_press(void)
 {
@@ -18,8 +22,60 @@ int key_press(void)
     if (str == NULL)
         return 84;
     tcgetattr(0, &termios);
-    tcsetattr(0, ICANON, &termios);
-    tcdrain(0);
+    termios.c_lflag&=~ICANON;
+    termios.c_lflag&=~INPCK;
+    termios.c_lflag&=~IXOFF;
+    termios.c_lflag&=~ECHO;
+    tcsetattr(0, TCSADRAIN, &termios);
     read(0, str, 1);
     return 0;
+}
+
+void egal_key(touch_t *touch, int i)
+{
+    touch->keys[i] = touch->touching[i][2];     
+    if (touch->keys[i] == 'D')
+        touch->keys[i] = KEY_LEFT;
+    if (touch->keys[i] == 'C')
+        touch->keys[i] = KEY_RIGHT;
+    if (touch->keys[i] == 'A')
+        touch->keys[i] = KEY_UP;
+    if (touch->keys[i] == 'B')
+        touch->keys[i] = KEY_DOWN;
+    
+}
+
+void take_key(touch_t *touch)
+{
+    int i = 0;
+    
+    touch->keys = malloc(sizeof(int) * 6);
+    for( ; i <= 5; i++) {
+        if (touch->touching[i][0] == 27)
+            egal_key(touch, i);
+        else if (my_strcmp(touch->touching[i], "(space)") == 0)
+        touch->keys[i] = KEY_BACKSPACE;
+        else
+            touch->keys[i] = touch->touching[i][0];
+    }
+}
+
+int switch_key(touch_t *touch, int getc)
+{
+    int *i = 0;
+    
+    take_key(touch);
+    switch(getc)
+    case touch->keys[left]:
+        break;
+    case touch->keys[right]:
+        break;
+    case touch->keys[turn]:
+        break;
+    case touch->keys[drop]:
+        break;
+    case touch->keys[quit]:
+        break;
+    case touch->keys[pose]:
+        break:
 }
--- src/main.c
+++ src/main.c
@@ -11,9 +11,12 @@
 int main(int ac, char **av)
 {
     touch_t touch;
-
+    int i = 0;
+    
     init_value(&touch);
     if (find_arg(ac, av, &touch) == 84)
         return 84;
+    key_press();
+    switch_key(&touch, i);
     return 0;
 }
